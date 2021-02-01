65fa07170076f1cd2854ec0c8e5e4b12abad42ed
Christian Stano <christian.p.stano@vanderbilt.edu>
2021-01-31 21:51:55 -0500

    Fixed IP mappings

--- main.tf
+++ main.tf
@@ -19,11 +19,11 @@ module "vpc" {
   region = "us-east-1"
   vpc_cidr = "10.0.0.0/16"
   private_subnet_cidr_blocks = [
-    "10.0.1.0/24",
+    "10.0.2.0/24",
     "10.0.3.0/24"]
   public_subnet_cidr_blocks = [
     "10.0.0.0/24",
-    "10.0.2.0/24"]
+    "10.0.1.0/24"]
   availability_zones = [
     "us-east-1a",
     "us-east-1b"]
